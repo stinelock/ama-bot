5c1208f0da3f33bee0abb2445c6961f8a3d4d4e8
stinelock <eaa24stlo@students.eaaa.dk>
2026-09-04 10:43:38 +0200

    feat: implement message handling and update form submission for chat interface

--- server.js
+++ server.js
@@ -6,20 +6,30 @@ const port = 8000;
 //----------------------MIDDLEWARE----------------------//
 
 app.use(express.static("public")); // Giver express adgang til min public mappe
+app.use(express.urlencoded({ extended: true })); // Giver express adgang til at parse data fra formular
 app.set("view engine", "ejs");
+
+//---------------------SAMTALEHISTORIK---------------------//
+
+const messages = [];
 
 //----------------------ROUTES----------------------//
 
-app.get("/", (req,res)=>{
-    res.render("index");
-})
+app.get("/", (req, res) => {
+  res.render("index", { messages });
+});
 
+app.post("/ask", (req, res) => {
+  const question = req.body.question;
 
+  messages.push({ type: "question", text: question });
+  messages.push({ type: "answer", text: "Jeg leder efter et svar ..." });
 
-
-
+  res.render("index", { messages });
+});
 
 //----------------------OPSTART AF SERVER----------------------//
 
-app.listen(port, ()=> {console.log(`Server is runnning at http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is runnning at http://localhost:${port}`);
+});
