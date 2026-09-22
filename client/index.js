90b83c1ba4269081250639d31cf1d61c35ba46aa
stinelock <eaa24stlo@students.eaaa.dk>
2026-09-22 12:47:49 +0200

    Add error paragraph ID and fix event listener case in chat form

--- client/index.js
+++ client/index.js
@@ -1,11 +1,17 @@
-const form = document.getElementById('chat-form');
+const form = document.getElementById("chat-form");
+
+form.addEventlistener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+	event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
 
 
-form.addEventlistener('submit', handleFormSubmit);
+	console.log("Form submitted");
+}
 
-
-function handleFormSubmit(event){
-    event.preventDefault();
-
-
-};
+handleFormSubmit();
