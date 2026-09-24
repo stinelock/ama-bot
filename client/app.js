5147a5b5fc3030bdf7f6b626821b653be2109563
stinelock <eaa24stlo@students.eaaa.dk>
2026-09-24 13:49:01 +0200

    Implement CORS middleware and enhance chat functionality with message fetching and display

--- client/app.js
+++ client/app.js
@@ -1,13 +1,44 @@
 const chatForm = document.getElementById("chat-form");
-const chatBox = document.querySelector(".chat-box");
+const chatSection = document.querySelector(".chat-section");
 const questionInput = document.getElementById("question");
 const sendBtn = document.getElementById("send-button");
 const clearBtn = document.getElementById("clear-chat-btn");
 
-console.log(
-	chatForm,
-	chatBox,
-	sendBtn,
-	clearBtn,
-    questionInput
-);
+const API_URL = "http://localhost:8000";
+
+async function getMessages() {
+	try {
+		const res = await fetch(`${API_URL}/messages`);
+
+		if (!res.ok) {
+			throw new Error(`Server error: ${res.status}`);
+		}
+
+		const messages = await res.json();
+
+        for (message of messages){
+            displayMessage(message)
+        }
+
+        console.log(messages);
+
+	} catch (error) {
+		console.log("fejl i fetch af data");
+	}
+}
+
+function displayMessage(message) {
+	const html = `<article class="chat-box">
+                    <div class="${message.type}">
+                        <p>${message.text}</p>
+                    </div>
+                    <p class="timestamp">${message.createdAt}</p>
+                </article>`;
+
+	chatSection.insertAdjacentHTML("beforeend", html);
+}
+
+displayMessage({ type: "question", text: "Test", createdAt: "now" });
+displayMessage({ type: "answer", text: "Test" });
+
+getMessages()
--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import messagesRouter from "./routes/messages.js";
 import answersRouter from "./routes/answers.js";
+import cors from "cors"
 
 const app = express();
 const port = 8000;
 
 //----------------------MIDDLEWARE----------------------//
 app.use(express.json());
+app.use(cors());
 
 app.use("/messages", messagesRouter);
 app.use("/answers", answersRouter);
